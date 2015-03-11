4bd2c04d77fd85142bc089e65e8f0e8c925dc151
Tony Li <jintli@redhat.com>
2015-03-11 15:49:47 -0400

    add staypuft.pp

---
+++ usr/share/foreman-installer/hooks/post/10-setup_provisioning.rb
@@ -0,0 +1,35 @@
+if app_value(:provisioning_wizard) != 'none' && [0,2].include?(kafo.exit_code)
+  require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'foreman.rb')
+  require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'base_seeder.rb')
+  require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'provisioning_seeder.rb')
+  require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'subscription_seeder.rb')
+
+  puts "Starting configuration..."
+
+  # we must enforce at least one puppet run
+  logger.debug 'Running puppet agent to seed foreman data'
+  `service puppet stop`
+  `puppet agent -t`
+  `service puppet start`
+  logger.debug 'Puppet agent run finished'
+
+  logger.debug 'Installing puppet modules'
+  `/usr/share/foreman-installer/hooks/lib/install_modules.sh`
+  `foreman-rake puppet:import:puppet_classes[batch]`
+  # run import
+  logger.debug 'Puppet modules installed'
+
+
+  # first, create RedHat installation media and subscription info
+  sub_seeder = SubscriptionSeeder.new(kafo)
+  sub_seeder.seed
+
+  # add other provisioning data
+  pro_seeder = ProvisioningSeeder.new(kafo)
+  pro_seeder.seed
+  `foreman-rake db:migrate`
+  `foreman-rake db:seed`
+else
+  say "Not running provisioning configuration since installation encountered errors, exit code was <%= color('#{kafo.exit_code}', :bad) %>"
+  false
+end
---
+++ usr/share/foreman-installer/modules/foreman/manifests/plugin/staypuft.pp
@@ -0,0 +1,132 @@
+# = Staypuft
+#
+# Installs staypuft package
+#
+# === Extra parameters:
+#
+# These parameters are not used for this class. They are placeholders for the installer
+# so it can store values in answer file
+#
+# $configure_networking:: Should local networking be configured by installer?
+#                         type:boolean
+#
+# $configure_firewall::   Should local firewall be configured by installer?
+#                         type:boolean
+#
+# $interface::            Which interface should this class configure
+#
+# $ip::                   What IP address should be set
+#
+# $netmask::              What netmask should be set
+#
+# $own_gateway::          What is the gateway for this machine
+#
+# $dns::                  DNS forwarder to use as secondary nameserver
+#
+# $network::              Network address used when seeding subnet in Foreman
+#
+# $from::                 DHCP range first address, used for DHCP configuration and
+#                         during Foreman subnet seeding
+#
+# $to::                   DHCP range last address, used for DHCP configuration and
+#                         during Foreman subned seeding
+#
+# $domain::               DNZ zone, used for DNS server configuration and during Foreman
+#                         Domain seeding
+#
+# $base_url::             URL of Foreman instance used for smart proxy configuration
+#
+# $gateway::              What is the gateway for machines using managed DHCP
+#
+# $ntp_host::             NTP sync host
+#
+# $timezone::             Timezone (IANA identifier)
+#
+# $root_password::        Default root password for provisioned machines
+#                         type:password
+#
+# $ssh_public_key::       SSH public key installed on provisioned machines during provisioning
+#
+class foreman::plugin::staypuft(
+    $configure_networking = true,
+    $configure_firewall = true,
+    $interface,
+    $ip,
+    $netmask,
+    $own_gateway,
+    $gateway,
+    $dns,
+    $network,
+    $from,
+    $to,
+    $domain,
+    $base_url,
+    $ntp_host,
+    $timezone,
+    $root_password,
+    $ssh_public_key
+) {
+  validate_bool($configure_networking)
+
+  case $::operatingsystem {
+    'fedora': {
+      $staypuft_name = 'rubygem-staypuft'
+    }
+    default: {
+      $staypuft_name = 'ruby193-rubygem-staypuft'
+    }
+  }
+
+  package { $staypuft_name:
+    notify  => Class['foreman::service'],
+    require => Exec['NTP sync'],
+  }
+
+  exec { 'NTP sync':
+    command => "/sbin/service ntpd stop; /usr/sbin/ntpdate $ntp_host",
+    notify  => Service['ntpd'],
+    require => [Package['ntp'], Package['ntpdate']],
+  }
+
+  package { ['ntp', 'ntpdate']: }
+
+  service { 'ntpd':
+    name   => 'ntpd',
+    ensure => 'running',
+    enable => true,
+  }
+
+  if $timezone {
+    case $::osfamily {
+      'RedHat': {
+        if ($::operatingsystem == 'Fedora' or
+           ($::operatingsystem != 'Fedora' and $::operatingsystemmajrelease > 6)) {
+          # EL 7 variants and Fedora
+          exec { 'set timezone':
+            command => "/bin/timedatectl set-timezone $timezone",
+          }
+        } else {
+          # EL 6 variants
+          exec { 'ensure selected timezone exists':
+            command => "/usr/bin/test -e /usr/share/zoneinfo/$timezone",
+          }
+
+          file { '/etc/localtime':
+            ensure  => 'file',
+            source  => "/usr/share/zoneinfo/$timezone",
+            replace => true,
+            require => Exec['ensure selected timezone exists'],
+          }
+
+          exec { 'set timezone in /etc/sysconfig/clock':
+            command => "/bin/sed -ie 's|^ZONE=.*$|ZONE=\"$timezone\"|' /etc/sysconfig/clock",
+            require => Exec['ensure selected timezone exists'],
+          }
+        }
+      }
+      default: {
+        fail("${::hostname}: Setting timezone not supported on osfamily ${::osfamily}")
+      }
+    }
+  }
+}
